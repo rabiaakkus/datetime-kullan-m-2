6e2bca7c516aa094050e82513a6641e3c31bbafc
rabiaakkus <145451092+rabiaakkus@users.noreply.github.com>
2024-01-02 11:13:41 +0300

    Add files via upload

---
+++ odev2 -1200/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace odev2__1200
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hesapla = new System.Windows.Forms.Button();
+            this.lblAy = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // hesapla
+            // 
+            this.hesapla.Location = new System.Drawing.Point(69, 53);
+            this.hesapla.Name = "hesapla";
+            this.hesapla.Size = new System.Drawing.Size(75, 23);
+            this.hesapla.TabIndex = 0;
+            this.hesapla.Text = "hesapla";
+            this.hesapla.UseVisualStyleBackColor = true;
+            this.hesapla.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblAy
+            // 
+            this.lblAy.AutoSize = true;
+            this.lblAy.Location = new System.Drawing.Point(28, 53);
+            this.lblAy.Name = "lblAy";
+            this.lblAy.Size = new System.Drawing.Size(18, 13);
+            this.lblAy.TabIndex = 1;
+            this.lblAy.Text = "ay";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAy);
+            this.Controls.Add(this.hesapla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button hesapla;
+        private System.Windows.Forms.Label lblAy;
+    }
+}
+
